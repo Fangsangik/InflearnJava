f6375c958d7117a1c28b1dd432c77b20187a1f42
sangikHwang <hwangsangik@gamil.com>
2024-04-22 13:03:31 +0900

    Java

---
+++ src/main/java/chap26/enumerate/practice3/HttpStatusMain.java
@@ -0,0 +1,19 @@
+package chap26.enumerate.practice3;
+
+import java.util.Scanner;
+
+public class HttpStatusMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("HTTP CODE : ");
+        int code = sc.nextInt();
+
+        HttpStatus status = HttpStatus.findCode(code);
+        if (status == null) {
+            System.out.println("정의되지 않은 코드");
+        } else {
+            System.out.println(status.getCode() + " " + status.getMessage());
+            System.out.println("isSuccess " + status.isSuccess());
+        }
+    }
+}
